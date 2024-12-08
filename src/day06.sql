2638f2c719bc609f3acd7cb4e01f72718ac8075d
Kamaruni <Christoph.Kober@software-craftsmen.at>
2024-12-08 08:20:05 +0100

    slow solution for day6

--- src/day06.sql
+++ src/day06.sql
@@ -1,138 +1,68 @@
 \i src/prelude.sql
 copy raw_lines (line) from '../input/day06.txt';
 
--- insert into raw_lines(line) values
--- ('....#.....'),
--- ('.........#'),
--- ('......#...'),
--- ('..#...^...'),
--- ('.......#..'),
--- ('..........'),
--- ('.#........'),
--- ('........#.'),
--- ('#.........'),
--- ('......#...');
+create type movement as (row int, col int, item text, direction text, step bigint, moved boolean, detected_loop boolean);
 
-with recursive map as (
-    select
-        line_no as row,
-        col,
-        item
-    from raw_lines,
-    lateral regexp_split_to_table(line, '') with ordinality cols(item, col)
-),
-movements as (
-    select
-        row,
-        col,
-        item as item,
-        'up' as direction,
-        1 as step,
-        true as moved
-    from map
-    where item = '^'
-    union all
-    select * from (
-        with previous as (
-            select * from movements
-        ),
-        next as (
-            select
-                next.row,
-                next.col,
-                next.item,
-                previous.direction as direction,
-                previous.step + 1 as step,
-                true as moved
-            from previous as previous, map as next
-            where previous.step = (select max(step) from previous)
-            and (next.row, next.col) = case previous.direction
-                when 'up' then (previous.row - 1, previous.col)
-                when 'right' then (previous.row, previous.col + 1)
-                when 'down' then (previous.row + 1, previous.col)
-                when 'left' then (previous.row, previous.col - 1)
-            end
-        ),
-        turn as (
-            select
-                previous.row,
-                previous.col,
-                previous.item,
-                case previous.direction
-                    when 'up' then 'right'
-                    when 'right' then 'down'
-                    when 'down' then 'left'
-                    when 'left' then 'up'
-                end as direciton,
-                previous.step + 1 as step,
-                false as moved
-            from previous
-            where previous.step = (select max(step) from previous)
-        )
-        select * from next where next.item <> '#'
-        union all
-        select * from turn where exists(select 1 from next where next.item = '#')
-    ) as next
+create or replace function next_coordinates(
+    previous_direction text,
+    previous_row int,
+    previous_col int,
+    out next_row int,
+    out next_col int
 )
-select count(distinct (row, col))
-from movements
-where moved;
+as $$
+begin
+    next_row = case previous_direction
+        when 'up' then previous_row - 1
+        when 'down' then previous_row + 1
+        else previous_row
+        end;
 
-create type movement as (row int, col int, item text, direction text, step integer, moved boolean);
+    next_col = case previous_direction
+        when 'left' then previous_col - 1
+        when 'right' then previous_col + 1
+        else previous_col
+    end;
+end
+$$ language plpgsql;
 
-create or replace function solvePart2() returns bigint as $$
+create or replace function determine_movements(
+    map text[][],
+    start_row int,
+    start_col int
+)
+returns setof movement
+as $$
 declare
-    original_map text[][];
-    start_row int;
-    start_col int;
     last_movement movement;
     next_movement movement;
     exited_map boolean;
+    detected_loop boolean;
     next_row int;
     next_col int;
     next_item text;
     next_direction text;
 begin
-    raise info 'start calculation';
-
-    original_map = (
-        select array_agg(regexp_split_to_array(line, ''))
-        from raw_lines
-    );
-
-    start_row = (select line_no from raw_lines where line like '%^%');
-    start_col = (select position('^' in line) from raw_lines where line like '%^%');
-
-    create temporary table movements
-    (row int, col int, item text, direction text, step bigint, moved boolean)
+    create temporary table if not exists movements
+    (row int, col int, item text, direction text, step bigint, moved boolean, detected_loop boolean)
     on commit drop;
+
+    truncate movements;
 
     last_movement = (start_row, start_col, '^', 'up', 1, true);
     insert into movements select (last_movement).*;
 
     exited_map = false;
-    while not exited_map loop
-        raise info '%', last_movement.direction;
+    detected_loop = false;
+    while not exited_map and not detected_loop loop
+        select * from next_coordinates(last_movement.direction, last_movement.row, last_movement.col)
+        into next_row, next_col;
 
-        next_row = case last_movement.direction
-            when 'up' then last_movement.row - 1
-            when 'down' then last_movement.row + 1
-            else last_movement.row
-        end;
+        next_item = map[next_row][next_col];
 
-        next_col = case last_movement.direction
-            when 'left' then last_movement.col - 1
-            when 'right' then last_movement.col + 1
-            else last_movement.col
-        end;
-
-        next_item = original_map[next_row][next_col];
-        raise info '% / % / %', next_row, next_col, next_item;
-
-        if last_movement.step > 6000 then
-            exited_map = true;
-        elseif next_item = '.' then
-            next_movement = (next_row, next_col, original_map[next_row][next_col], last_movement.direction, last_movement.step + 1, true);
+        if next_item in ('.', '^') then
+            detected_loop = exists(select 1 from movements as m where (m.direction, m.row, m.col) = (last_movement.direction, next_row, next_col));
+            next_movement = (next_row, next_col, map[next_row][next_col], last_movement.direction, last_movement.step + 1, true, detected_loop);
             insert into movements select (next_movement).*;
         elseif next_item = '#' then
             next_direction = case last_movement.direction
@@ -141,7 +71,7 @@ begin
                 when 'down' then 'left'
                 when 'left' then 'up'
             end;
-            next_movement = (last_movement.row, last_movement.col, last_movement.item, next_direction, last_movement.step + 1, false);
+            next_movement = (last_movement.row, last_movement.col, last_movement.item, next_direction, last_movement.step + 1, false, detected_loop);
             insert into movements select (next_movement).*;
         else
             exited_map = true;
@@ -150,8 +80,93 @@ begin
         last_movement = next_movement;
     end loop;
 
-    return (select count(distinct (row, col)) from movements where moved);
+    return query select * from movements;
 end
 $$ language plpgsql;
 
-select * from solvePart2();
+-- slow - takes about 1 minute
+-- the equivalent implementation with `with recursive` takes a full minute!
+create or replace function solve_part_1()
+returns bigint
+as $$
+declare
+    map text[][];
+    start_row int;
+    start_col int;
+begin
+    map = (
+        select array_agg(regexp_split_to_array(line, ''))
+        from raw_lines
+    );
+
+    start_row = (select line_no from raw_lines where line like '%^%');
+    start_col = (select position('^' in line) from raw_lines where line like '%^%');
+
+    return (
+        select count(distinct (row, col))
+        from determine_movements(map, start_row, start_col)
+        where moved
+    );
+end
+$$ language plpgsql;
+
+-- very slow - takes about 1 hour
+create or replace function solve_part_2()
+    returns bigint
+as $$
+declare
+    map text[][];
+    modified_map text[][];
+    start_row int;
+    start_col int;
+    obstruction record;
+    result bigint;
+    curr bigint;
+    total bigint;
+begin
+    map = (
+        select array_agg(regexp_split_to_array(line, ''))
+        from raw_lines
+    );
+
+    start_row = (select line_no from raw_lines where line like '%^%');
+    start_col = (select position('^' in line) from raw_lines where line like '%^%');
+
+    create temporary table original_movements
+    (row int, col int, item text, direction text, step bigint, moved boolean)
+    on commit drop;
+
+    insert into original_movements
+    select row, col, item, direction, step, moved from determine_movements(map, start_row, start_col);
+
+    create temporary table potential_obstructions
+    (row int, col int)
+    on commit drop;
+
+    insert into potential_obstructions
+    select distinct c.next_row, c.next_col
+    from original_movements as o,
+    lateral (select * from next_coordinates(o.direction, o.row, o.col)) as c
+    where map[c.next_row][c.next_col] = '.';
+
+    total = (select count(*) from potential_obstructions);
+
+    result = 0;
+    curr = 0;
+    for obstruction in (select * from potential_obstructions) loop
+        curr = curr + 1;
+	raise info 'analyzing % / % (%)', curr, total, result;
+	modified_map = array_cat(map, '{}'::text[][]);
+        modified_map[obstruction.row][obstruction.col] = '#';
+        if (select bool_or(detected_loop) from determine_movements(modified_map, start_row, start_col)) then
+            result = result + 1;
+        end if;
+    end loop;
+
+    return result;
+end
+$$ language plpgsql;
+
+select
+    (select * from solve_part_1()) as solution_part_1,
+    (select * from solve_part_2()) as solution_part_2;
